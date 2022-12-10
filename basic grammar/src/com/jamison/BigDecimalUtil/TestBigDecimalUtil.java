4957e665d9bdf5708d7e95f49b0428b4ade8e406
jamison <2810947907@qq.com>
2022-12-10 13:26:22 +0800

    Test BigDecimalUtil

---
+++ basic grammar/src/com/jamison/BigDecimalUtil/TestBigDecimalUtil.java
@@ -0,0 +1,14 @@
+package com.jamison.BigDecimalUtil;
+
+import java.math.BigDecimal;
+
+/**
+ * 测试BigDecimal工具类
+ * @author jamison
+ */
+public class TestBigDecimalUtil {
+    public static void main(String[] args) {
+        System.out.println(BigDecimalUtil.add(2.5, 6.7));
+        System.out.println(BigDecimalUtil.sub(2.5, 6.7));
+    }
+}
