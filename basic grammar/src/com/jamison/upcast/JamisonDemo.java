7a71840232b29b5f2ed1c2d5befb3821c9ba41e9
jamison <2810947907@qq.com>
2023-01-18 11:24:02 +0800

    upcast

---
+++ basic grammar/src/com/jamison/upcast/JamisonDemo.java
@@ -0,0 +1,33 @@
+package com.jamison.upcast;
+class Book {
+    public String getInfo() {
+        return "[book]这是一本书";
+    }
+}
+
+class MathBook extends Book {
+    public String getInfo() {
+        return "[MathBook]这是一本书";
+    }
+}
+
+class ProgramBook extends Book {
+    public String getInfo() {
+        return "[ProgramBook]这是一本书";
+    }
+}
+
+
+/**
+ * @author jamison
+ */
+public class JamisonDemo {
+    public static void main(String[] args) {
+        fun(new MathBook());
+        fun(new ProgramBook());
+    }
+
+    public static void fun(Book book) {
+        System.out.println(book.getInfo());
+    }
+}
