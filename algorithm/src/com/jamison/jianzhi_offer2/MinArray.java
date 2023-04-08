059c29e2cf18713cf67667ebde6081af192c8db6
jamison <2810947907@qq.com>
2023-04-08 15:05:07 +0800

    minArray-二分

--- algorithm/src/com/jamison/jianzhi_offer2/MinArray.java
+++ algorithm/src/com/jamison/jianzhi_offer2/MinArray.java
@@ -1,14 +1,21 @@
 package com.jamison.jianzhi_offer2;
 //剑指 Offer 11. 旋转数组的最小数字
 
-//暴力求解
+//二分法
 public class MinArray {
     public int minArray(int[] numbers) {
-        for(int i = 0; i < numbers.length - 1; i++) {
-            if(numbers[i] - numbers[i + 1] > 0) {
-                return numbers[i + 1];
+        int left  = 0;
+        int right = numbers.length - 1;
+        while(left < right) {
+            int m = left + (right - left) / 2;
+            if(numbers[m] > numbers[right]) {
+                left = m + 1;
+            } else if(numbers[m] < numbers[right]) {
+                right = m;
+            } else {
+                right--;
             }
         }
-        return numbers[0];
+        return numbers[left];
     }
 }
