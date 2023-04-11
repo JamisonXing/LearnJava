946eeaed5a227fa5e71217cd0472615b431f3b72
jamison <2810947907@qq.com>
2023-04-11 23:46:19 +0800

    hammingWeight

---
+++ algorithm/src/com/jamison/jianzhi_offer2/HammingWeight.java
@@ -0,0 +1,12 @@
+package com.jamison.jianzhi_offer2;
+
+public class HammingWeight {
+    public int hammingWeight(int n) {
+        int res = 0;
+        while(n != 0) {
+            res += n & 1;
+            n >>>= 1;
+        }
+        return res;
+    }
+}
