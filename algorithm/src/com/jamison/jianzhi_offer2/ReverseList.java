d45ab628c61fed18222516cf1b031c98e4a88d0c
jamison <2810947907@qq.com>
2023-04-16 13:42:39 +0800

    reverseList2

--- algorithm/src/com/jamison/jianzhi_offer2/ReverseList.java
+++ algorithm/src/com/jamison/jianzhi_offer2/ReverseList.java
@@ -4,13 +4,13 @@ import java.util.List;
 
 public class ReverseList {
     public ListNode reverseList(ListNode head) {
-        ListNode cur = head, pre = null, tmp = null;
-        while(cur != null) {
-            tmp = cur.next;
-            cur.next = pre;
-            pre = cur;
-            cur = tmp;
+        ListNode cur = null, pre = head;
+        while(pre != null) {
+            ListNode tmp = pre.next;
+            pre.next = cur;
+            cur = pre;
+            pre = tmp;
         }
-        return pre;
+        return cur;
     }
 }
