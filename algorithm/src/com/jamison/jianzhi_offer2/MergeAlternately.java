d5f949e6a4fa94dc3398e8375b535efd266de5a3
jamison <2810947907@qq.com>
2023-04-24 09:10:16 +0800

    mergeAlternately

---
+++ algorithm/src/com/jamison/jianzhi_offer2/MergeAlternately.java
@@ -0,0 +1,23 @@
+package com.jamison.jianzhi_offer2;
+
+import java.util.ArrayList;
+
+public class MergeAlternately {
+    public String mergeAlternately(String word1, String word2) {
+        int m = word1.length(), n = word2.length();
+        int i = 0, j = 0;
+        StringBuilder res = new StringBuilder();
+        while (i < m || j < n) {
+            if(i < m) {
+                res.append(word1.charAt(i));
+                ++i;
+            }
+            if(j < n) {
+                res.append(word2.charAt(j));
+                ++j;
+            }
+        }
+
+        return res.toString();
+    }
+}
