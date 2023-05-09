2792d6289f5a2d4d382337a038dcdf8c4aeda792
jamison <2810947907@qq.com>
2023-05-09 10:34:35 +0800

    reverseWords

---
+++ algorithm/src/com/jamison/leetCode75/ReverseWords.java
@@ -0,0 +1,18 @@
+package com.jamison.leetCode75;
+
+import java.util.*;
+
+public class ReverseWords {
+    public static void main(String[] args) {
+        String s = "the sky is blue";
+        System.out.println(reverseWords(s));
+    }
+    static public String reverseWords(String s) {
+        String sTrim = s.trim();
+        String[] strings = sTrim.split(" ");
+        List<String> wordList = new ArrayList<>();
+        Collections.addAll(wordList, strings);
+        Collections.reverse(wordList);
+        return  String.join(" ", wordList);
+    }
+}
